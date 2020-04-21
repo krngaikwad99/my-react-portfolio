4b9c4d733060791007e8cb7cb1007bdb3843df36
Karan Gaikwad <63577658+krngaikwad99@users.noreply.github.com>
2020-04-21 19:13:05 -0400

    Updates

---
+++ precache-manifest.223b46e51ebf980cf90fcf158b9eb7dc.js
@@ -0,0 +1,50 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "36edf500e026d506669688e4afedd3ee",
+    "url": "/my-react-portfolio/index.html"
+  },
+  {
+    "revision": "091fbf51dc1dbb71eeae",
+    "url": "/my-react-portfolio/static/css/2.b6a8c48d.chunk.css"
+  },
+  {
+    "revision": "c9928cd4d2a044a5ea33",
+    "url": "/my-react-portfolio/static/css/main.b11396c8.chunk.css"
+  },
+  {
+    "revision": "091fbf51dc1dbb71eeae",
+    "url": "/my-react-portfolio/static/js/2.cc7bdf87.chunk.js"
+  },
+  {
+    "revision": "e1d2b252d73a4c2bc4d6802ca4ba1aa0",
+    "url": "/my-react-portfolio/static/js/2.cc7bdf87.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "c9928cd4d2a044a5ea33",
+    "url": "/my-react-portfolio/static/js/main.159f5f90.chunk.js"
+  },
+  {
+    "revision": "0c530593fa9a17e82b1e",
+    "url": "/my-react-portfolio/static/js/runtime-main.dee2495f.js"
+  },
+  {
+    "revision": "7a60be73cbd8cd37d15c4bcf27f081c2",
+    "url": "/my-react-portfolio/static/media/CRinterface.7a60be73.png"
+  },
+  {
+    "revision": "c2c3745b26a042f446d03c531793f3c4",
+    "url": "/my-react-portfolio/static/media/PortraitPic.c2c3745b.png"
+  },
+  {
+    "revision": "0e9869c29fb2213d47dc0689e4136af6",
+    "url": "/my-react-portfolio/static/media/WebDes.0e9869c2.png"
+  },
+  {
+    "revision": "2890864461a39f94790c8dd2864802e4",
+    "url": "/my-react-portfolio/static/media/WebDev.28908644.png"
+  },
+  {
+    "revision": "77b45aa026bbd3701989bf7caba4672e",
+    "url": "/my-react-portfolio/static/media/bitmojiface.77b45aa0.png"
+  }
+]);
